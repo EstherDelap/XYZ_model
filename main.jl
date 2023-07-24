dbc9aa6184c01c5f6886a7f6334c1427a21611e9
EstherDelap <py20ed@leeds.ac.uk>
2023-07-24 15:12:05 +0100

    changed number of repeats

--- main.jl
+++ main.jl
@@ -4,7 +4,7 @@ using JLD2
 function main()
     #commit_id = readchomp(`git rev-parse HEAD`)
     dim = (4,4,4)
-    N = (1000)
+    N = 1000
     number_repeats = 1000
     Γ_deph = 0
     Γ_decay = 0.025
